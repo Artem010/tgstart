29b130a5ae442dc6b14abb234825945656467be1
artem010 <cmert.123@yandex.ru>
2021-01-17 15:53:14 +0500

    x

--- tgstart/tgstart2/urls.py
+++ tgstart/tgstart2/urls.py
@@ -12,38 +12,29 @@ urlpatterns = [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # START SUBPROCESSES
-from django.db import models
-from users.models import User
-import os, subprocess
-
-cUsers = User.objects.all()
-for u in cUsers:
-    cUserId = u.id
-    print('user: ' + str(u))
-    cBots = u.bot_set.all()
-    print('bot count: ' + str(cBots.count()))
-    if(cBots.count()>0):
-        print('*****')
-        for b in cBots:
-            cBotId = b.id
-            print('bot id: ' + str(b.id))
-            cBotStatus = b.status
-            print('bot status: ' + str(b.status))
-
-            if(cBotStatus == 1):
-                cDir = os.getcwd() + '/tgstart2/bots/' + str( cUserId) + "/"+str(cBotId)
-                print (cDir +'/main.py')
-                pr = subprocess.Popen(['python', cDir +'/main.py'])
-                print('pID ' + str(pr.pid))
-
-                u.bot_set.filter(id=cBotId).update(pID = pr.pid, status = 1)
-        print('*****')
-
-# cBotId =request.GET['id']
-# cBot = cUser.bot_set.get(id = cBotId)
-# cDir = os.getcwd() + '/tgstart2/bots/' + str( request.session.get('sUserId')) + "/"+str(cBotId)
-# print (cDir + str(request.session.get('sUserId')) + "/" + str(cBotId) +'/main.py')
-# pr = subprocess.Popen(['python', cDir +'/main.py'])
-# print('pID ' + str(pr.pid))
+# from django.db import models
+# from users.models import User
+# import os, subprocess
 #
-# cUser.bot_set.filter(id=cBotId).update(pID = pr.pid, status = 1)
+# cUsers = User.objects.all()
+# for u in cUsers:
+#     cUserId = u.id
+#     print('user: ' + str(u))
+#     cBots = u.bot_set.all()
+#     print('bot count: ' + str(cBots.count()))
+#     if(cBots.count()>0):
+#         print('*****')
+#         for b in cBots:
+#             cBotId = b.id
+#             print('bot id: ' + str(b.id))
+#             cBotStatus = b.status
+#             print('bot status: ' + str(b.status))
+#
+#             if(cBotStatus == 1):
+#                 cDir = os.getcwd() + '/tgstart2/bots/' + str( cUserId) + "/"+str(cBotId)
+#                 print (cDir +'/main.py')
+#                 pr = subprocess.Popen(['python', cDir +'/main.py'])
+#                 print('pID ' + str(pr.pid))
+#
+#                 u.bot_set.filter(id=cBotId).update(pID = pr.pid, status = 1)
+#         print('*****')
