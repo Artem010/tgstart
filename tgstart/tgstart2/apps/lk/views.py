8c8eedfa63d0e6999bda31962fa0ec7e9c0190c5
artem010 <cmert.123@yandex.ru>
2021-01-18 17:48:00 +0500

    added icons

--- tgstart/tgstart2/apps/lk/views.py
+++ tgstart/tgstart2/apps/lk/views.py
@@ -163,8 +163,9 @@ def mybots(request):
 
 def pay(request):
     return render(request, 'volt/pay.html', check_auth(request))
+
 def sidebar(request):
-    return render(request, 'volt/sidebar.html',check_auth(request))
+    return render(request, 'volt/sidebar.html')
 
 def senders(request):
     return render(request, 'volt/senders.html', check_auth(request))
