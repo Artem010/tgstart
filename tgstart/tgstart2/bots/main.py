1e17410b8853fcc14b468369b763d7466ee6b4ad
artem010 <cmert.123@yandex.ru>
2021-01-20 23:16:17 +0500

    code optimization main.py

--- tgstart/tgstart2/bots/main.py
+++ tgstart/tgstart2/bots/main.py
@@ -1,22 +1,14 @@
 import sys,os
 sys.path.insert(0, os.getcwd() + '/tgstart2/bots/')
-import config
-import telebot
-import sqlite3
-import json
-import dbAll
+import config,telebot,dbAll
 
 bot = telebot.TeleBot(config.token)
-cDir = os.getcwd() + '/tgstart2/bots/' + config.user_id + "/" + config.bot_id
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     user_profile = bot.get_user_profile_photos(message.from_user.id);
-    # print()
     if(user_profile.total_count > 0):
-        file_id= user_profile.photos[0][0].file_id
-        file_info = bot.get_file(file_id);
-        file_path = file_info.file_path
+        file_path = (bot.get_file(user_profile.photos[0][0].file_id)).file_path;
     else:
         file_path = "None"
 
