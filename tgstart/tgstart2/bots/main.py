ac5d1a590c0aca72da97e646bd6251afc65f236b
artem010 <cmert.123@yandex.ru>
2021-01-21 21:36:23 +0500

    fixed bug (keyboard tg)

--- tgstart/tgstart2/bots/main.py
+++ tgstart/tgstart2/bots/main.py
@@ -10,29 +10,31 @@ records = dbAll.getCustomCommandDB(config);
 # print(records)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True,True)
-
+hideBoard = telebot.types.ReplyKeyboardRemove()
 # keyboardDB = ['fg','gfd']
 
-print(len(records))
-i = 0
+print('records: '+str(records))
+if(records != 0):
+    i = 0
 
-while i < len(records):
-    try:
-        btn1 = records[i][1]
-    except Exception as e:
-        btn1 = ''
-    try:
-        btn2 = records[i+1][1]
-    except Exception as e:
-        btn2 = ''
-    try:
-        btn3 = records[i+2][1]
-    except Exception as e:
-        btn3 = ''
+    while i < len(records):
+        try:
+            btn1 = records[i][1]
+        except Exception as e:
+            btn1 = ''
+        try:
+            btn2 = records[i+1][1]
+        except Exception as e:
+            btn2 = ''
+        try:
+            btn3 = records[i+2][1]
+        except Exception as e:
+            btn3 = ''
 
-    keyboard1.row(btn1,btn2,btn3)
-    i+=3
-
+        keyboard1.row(btn1,btn2,btn3)
+        i+=3
+else:
+    keyboard1.add('')
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
@@ -48,12 +50,16 @@ def start_handler(message):
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     i=0;
-    for r in records:
-        if(message.text==r[1]):
-            i=1;
-            bot.send_message(message.chat.id, text=r[2], reply_markup=keyboard1)
-    if i == 0:
-        bot.send_message(message.chat.id, message.text, reply_markup=keyboard1)
+    if(records != 0):
+        for r in records:
+            if(message.text==r[1]):
+                i=1;
+                bot.send_message(message.chat.id, text=r[2], reply_markup=keyboard1)
+        if i == 0:
+            bot.send_message(message.chat.id, message.text, reply_markup = keyboard1)
+    else:
+        bot.send_message(message.chat.id, message.text, reply_markup=hideBoard)
+
 
 
     dbAll.addMsgsDB(config)
